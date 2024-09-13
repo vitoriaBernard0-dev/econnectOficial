e2091e85401531872580b243a786e34f41c62c48
vitoriaBernard0-dev <vi.bernardo12@gmail.com>
2024-09-13 12:29:30 -0300

    phpmyadim

---
+++ public/process_form.php
@@ -0,0 +1,38 @@
+<?php
+// Conectar ao banco de dados
+$hostname = 'localhost';
+$bancodedados = 'econnect';
+$usuario = 'root';
+$senha = '';
+
+// Criando a conexão
+$conn = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+// Verificar a conexão
+if ($conn->connect_error) {
+  die("Falha na conexão: " . $conn->connect_error);
+}
+
+// Receber dados do formulário
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Inserir os dados na tabela faleconosco
+$sql = "INSERT INTO faleconosco (faleconosco_name, faleconosco_email, faleconosco_message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<script>
+          alert('Mensagem enviada com sucesso!');
+          window.location.href = 'index.html'; // Redireciona para a página inicial
+        </script>";
+} else {
+  echo "<script>
+          alert('Erro: " . $conn->error . "');
+          window.location.href = 'index.html'; // Redireciona para a página inicial
+        </script>";
+}
+
+// Fechar a conexão
+$conn->close();
+?>
